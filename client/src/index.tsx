78732dc2f51cdec2ea045beb2ca3b855086a8bc9
eyyytran <ltatran94@gmail.com>
2022-11-13 15:37:03 -0500

    upgrade to react router v6

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App'
+import ErrorPage from './routes/ErrorPage'
+import Root from './routes/Root'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 )
